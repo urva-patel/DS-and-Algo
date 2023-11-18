adb07477a5cb50868e2532af9b38ba6e623950e1
Urva <Urvapatel1@gmail.com>
2023-11-18 13:35:24 -0500

    Time: 48 ms (6.54%), Space: 16.4 MB (18.62%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        opens = ['(', '{', '[']
+        openSet = set(opens)
+        stack = []
+        for p in s:
+            if p in opens:
+                stack.append(p)
+            else:
+                if len(stack) == 0:
+                    return False
+                potOpen = stack.pop()
+                if p == ')' and potOpen != '(':
+                    return False
+                if p == ']' and potOpen != '[':
+                    return False
+                if p == '}' and potOpen != '{':
+                    return False
+        if len(stack) > 0:
+            return False
+        return True
+                
+                    
