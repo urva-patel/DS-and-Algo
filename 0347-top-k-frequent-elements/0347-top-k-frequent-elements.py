0918602488b72c226a7488a310b3df0b23515403
Urva <Urvapatel1@gmail.com>
2023-12-11 19:02:03 -0500

    Time: 95 ms (75.57%), Space: 21 MB (58.50%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,18 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq_table = Counter(nums)
+
+        heap = []
+
+        for i in freq_table.keys():
+            if len(heap) == k:
+                heappushpop(heap, (freq_table[i], i))
+            else:
+                heappush(heap, (freq_table[i], i))
+        
+        ans = []
+
+        while k > 0:
+            k -= 1
+            ans.append(heappop(heap)[1])
+        return ans
